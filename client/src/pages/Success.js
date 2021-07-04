079d09f7d8c6b9b993f0cb8f6f62ea64d9bb7ee6
Senait <senutekie77@gmail.com>
2021-07-04 13:25:38 -0700

    success js fixed

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,43 +1,46 @@
-import React, { useEffect } from 'react';
-import { useMutation } from '@apollo/client';
-import Jumbotron from '../components/Jumbotron';
-import { ADD_ORDER } from '../utils/mutations';
-import { idbPromise } from '../utils/helpers';
+import React, { useEffect } from "react";
+import Jumbotron from "../components/Jumbotron";
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_ORDER } from "../utils/mutations.js";
+import { idbPromise } from "../utils/helpers";
 
 function Success() {
-  const [addOrder] = useMutation(ADD_ORDER);
+    const [addOrder] = useMutation(ADD_ORDER);
 
-  useEffect(() => {
+    useEffect(() => {
     async function saveOrder() {
-      const cart = await idbPromise('cart', 'get');
-      const products = cart.map((item) => item._id);
+        const cart = await idbPromise('cart', 'get');
+        const products = cart.map(item => item._id);
 
-      if (products.length) {
-        const { data } = await addOrder({ variables: { products } });
-        const productData = data.addOrder.products;
+        if (products.length) {
+            const { data } = await addOrder({ variables: { products } });
+            const productData = data.addOrder.products;
+          
+            productData.forEach((item) => {
+              idbPromise('cart', 'delete', item);
+            });
+          }
 
-        productData.forEach((item) => {
-          idbPromise('cart', 'delete', item);
-        });
-      }
-
-      setTimeout(() => {
-        window.location.assign('/');
-      }, 3000);
     }
 
     saveOrder();
-  }, [addOrder]);
+    }, [addOrder]);
 
-  return (
-    <div>
-      <Jumbotron>
-        <h1>Success!</h1>
-        <h2>Thank you for your purchase!</h2>
-        <h2>You will now be redirected to the home page</h2>
-      </Jumbotron>
-    </div>
-  );
-}
+    setTimeout(function(){ window.location.assign("/")}, 3000);
+
+    return (
+      <div>
+        <Jumbotron>
+          <h1>Success!</h1>
+          <h2>
+            Thank you for your purchase!
+          </h2>
+          <h2>
+            You will now be redirected to the homepage
+          </h2>
+        </Jumbotron>
+      </div>
+    );
+  };
 
 export default Success;
