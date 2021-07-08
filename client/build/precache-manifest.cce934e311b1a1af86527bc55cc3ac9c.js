2aefc6682006f39362bca5f61695fbd8f1c7dff7
Senait <senutekie77@gmail.com>
2021-07-07 17:28:01 -0700

    check the build folder

---
+++ client/build/precache-manifest.cce934e311b1a1af86527bc55cc3ac9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ad41a51675c95e02772ae17a9d5195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f59d8881fab7ddc69432",
+    "url": "/static/css/main.89228a49.chunk.css"
+  },
+  {
+    "revision": "139d25a698fefe15f2d1",
+    "url": "/static/js/2.60c82aac.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.60c82aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f59d8881fab7ddc69432",
+    "url": "/static/js/main.50cf577e.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
