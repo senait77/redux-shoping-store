903b9bd8aebe195f0e2b546a7455d6ef56db5f58
Senait <senutekie77@gmail.com>
2021-07-11 14:46:25 -0700

    server fixed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
 
-const MONGODB_ENDPOINT =
-    process.env.MONGODB_ENDPOINT ||
-    console.log(redText, 'You must have a Mongo Database endpoint.');
-
-mongoose.connect(MONGODB_ENDPOINT, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
-
-module.exports = mongoose.connection;
+const mongoose = require('mongoose')
+const connectToDB = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb+srv://nenu:8BHLtC9F40pTWt2E@cluster0.nrm88.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (err) {
+    console.log(`MongoDB Connection Error: ${err.message}`)
+  }
+}
+connectToDB()
+module.exports = mongoose.connection
